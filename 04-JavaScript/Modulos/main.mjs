b16db64724dfd3c409f149832a065f8e4460cfa4
EvertonTrennepohl <gauchotp@gmail.com>
2022-07-21 13:06:42 -0300

    Módulos JavaScript

---
+++ 04-JavaScript/Modulos/main.mjs
@@ -0,0 +1,7 @@
+import { mostraIdade, mostraCidade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Paulo", 35));
+
+console.log(mostraCidade("Paulo", "Humaitá"));
+
+console.log(mostraHobby("Paulo", "jogar futebol"));
